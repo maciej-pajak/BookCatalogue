f787581c2002fbcabda177f65db5d144d50ed172
maciej-pajak <maciek.pajak@vp.pl>
2018-02-21 10:47:28 +0100

    add cors

--- src/main/java/pl/maciejpajak/AppConfig.java
+++ src/main/java/pl/maciejpajak/AppConfig.java
@@ -2,12 +2,19 @@ package pl.maciejpajak;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="pl.maciejpajak")
+@ComponentScan(basePackages = "pl.maciejpajak")
 public class AppConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("http://localhost");
+    }
 }
